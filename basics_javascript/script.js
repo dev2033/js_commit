b25dab1f86adea5d9b89076c04901db1cf929e85
dev2033 <dev.elop.02@mail.ru>
2021-06-20 11:23:06 +0300

    upd

--- basics_javascript/script.js
+++ basics_javascript/script.js
@@ -1,19 +1,34 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+/**
+ * Простая работа с объектом START
+ */ 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Один из просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(personalMovieDB);
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
+
+// const a = prompt('Один из просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его', ''),
+//       c = prompt('Один из просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+/**
+ * Простая работа с объектом STOP
+ */
+
+
+/**
+ * 
+ */
